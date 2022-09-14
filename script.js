46336883b3855cbb25b06a13d598424dc3e3aea1
minhajmia <minhajmia418@gmail.com>
2022-09-14 22:15:05 +0600

    show typing spent issue completed

--- script.js
+++ script.js
@@ -137,7 +137,7 @@ displayHistory();
 // Show typing time spent
 setInterval(() => {
   const currentTime = new Date().getTime();
-  const timeSpent = (currentTime - startTime) / 1000;
+  const timeSpent = ((currentTime - startTime) / 1000).toFixed(0);
 
   document.getElementById("show-time").innerHTML = `${
     startTime ? timeSpent : 0
